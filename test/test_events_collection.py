90afae0142b3de48ae90d02a7ef4caf6fd5c3323
utak3r <piotr.borys@gmail.com>
2025-04-16 19:17:23 +0200

    Some more work on Event class

--- test/test_events_collection.py
+++ test/test_events_collection.py
@@ -7,14 +7,14 @@ def test_event_creation():
     start = datetime.datetime.now()
     end = datetime.datetime.now() + datetime.timedelta(hours=1)
     event = Event(name, start, end)
-    assert event.name == name
-    assert event.dateStart == start
-    assert event.dateEnd == end
+    assert event.name() == name
+    assert event.start() == start
+    assert event.end() == end
 
     event = Event()
-    assert event.name == ""
-    assert event.dateStart == None
-    assert event.dateEnd == None
+    assert event.name() == ""
+    assert event.start() == None
+    assert event.end() == None
 
     end2 = datetime.datetime.now() + datetime.timedelta(hours=-1)
     with pytest.raises(EventException):
@@ -25,8 +25,52 @@ get_duration_test_data = [
     (None, None, 0)
 ]
 @pytest.mark.parametrize("start, end, expected_duration", get_duration_test_data)
-def test_get_duration(start, end, expected_duration):
+def test_duration(start, end, expected_duration):
     name = "test 1"
     event = Event(name, start, end)
-    duration = event.get_duration()
+    duration = event.duration()
     assert duration.seconds == expected_duration
+
+simple_getters_test_data = [
+    ("test 1", datetime.datetime.now(), datetime.datetime.now() + datetime.timedelta(hours=1)),
+    ("test 2", None, None),
+    ("", None, None)
+]
+@pytest.mark.parametrize("name, start, end", simple_getters_test_data)
+def test_simple_getters(name, start, end):
+    event = Event(name, start, end)
+    assert event.name() == name
+    assert event.start() == start
+    assert event.end() == end
+
+def test_datetime_isoformat():
+    name = "test 1"
+    start = datetime.datetime(2025, 4, 16, 18, 28)
+    end = datetime.datetime(2025, 4, 16, 19, 0)
+    event = Event(name, start, end)
+    assert event.start_iso() == "2025-04-16T18:28:00"
+    assert event.end_iso() == "2025-04-16T19:00:00"
+
+get_as_text_test_data = [
+    ("test 1", 2025, 4, 16, 18, 28, 2025, 4, 16, 19, 0, "test 1 (2025-04-16, 18:28 - 2025-04-16, 19:00)"),
+    ("test 2", 2025, 4, 16, 18, 28, 0, 4, 16, 19, 0, "test 2 (2025-04-16, 18:28)"),
+    ("test 3", 0, 4, 16, 18, 28, 2025, 4, 16, 19, 0, "test 3 ( - 2025-04-16, 19:00)"),
+    ("test 4", 0, 4, 16, 18, 28, 0, 4, 16, 19, 0, "test 4"),
+]
+@pytest.mark.parametrize("name, " \
+    "start_year, start_month, start_day, start_hour, start_minute, " \
+    "end_year, end_month, end_day, end_hour, end_minute," \
+    "expected_string", get_as_text_test_data)
+def test_get_as_text(name, 
+                     start_year, start_month, start_day, start_hour, start_minute,
+                     end_year, end_month, end_day, end_hour, end_minute,
+                     expected_string):
+    name = name
+    start = None
+    end = None
+    if (start_year > 0):
+        start = datetime.datetime(start_year, start_month, start_day, start_hour, start_minute)
+    if (end_year > 0):
+        end = datetime.datetime(end_year, end_month, end_day, end_hour, end_minute)
+    event = Event(name, start, end)
+    assert event.get_as_text() == expected_string
--- u3SportCalendar/Events.py
+++ u3SportCalendar/Events.py
@@ -8,19 +8,47 @@ class EventException(Exception):
 
 class Event:
     def __init__(self, name:str="", start:datetime=None, end:datetime=None):
-        self.name = name
-        self.dateStart = start
-        self.dateEnd = end
+        self.the_name = name
+        self.the_start = start
+        self.the_end = end
         if (start is not None and end is not None):
             if (end < start):
-                self.dateEnd = start
+                self.the_end = start
                 raise EventException("End date is sooner than start date.")
             
-    def get_duration(self) -> datetime.timedelta:
+    def name(self) -> str:
+        return self.the_name
+    
+    def start(self) -> datetime:
+        return self.the_start
+    
+    def start_iso(self) -> str:
+        return self.the_start.isoformat(timespec='seconds')
+    
+    def end(self) -> datetime:
+        return self.the_end
+
+    def end_iso(self) -> str:
+        return self.the_end.isoformat(timespec='seconds')
+
+    def duration(self) -> datetime.timedelta:
         duration = datetime.timedelta()
-        if (self.dateStart is not None and self.dateEnd is not None):
-            duration = self.dateEnd - self.dateStart
+        if (self.the_start is not None and self.the_end is not None):
+            duration = self.the_end - self.the_start
         return duration
+    
+    def get_as_text(self) -> str:
+        text = ""
+        if (self.the_name != ""):
+            text += self.the_name
+        dates_text = ""
+        if (self.the_start is not None):
+            dates_text += self.the_start.strftime("%Y-%m-%d, %H:%M")
+        if (self.the_end is not None):
+            dates_text += self.the_end.strftime(" - %Y-%m-%d, %H:%M")
+        if (dates_text != ""):
+            text += " (" + dates_text + ")"
+        return text
     
 
 class EventsList:
