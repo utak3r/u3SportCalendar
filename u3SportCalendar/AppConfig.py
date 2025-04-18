75b4f119f8fd2cf30bdc468126c4186bc1ed16ba
utak3r <piotr.borys@gmail.com>
2025-04-18 02:00:55 +0200

    AppConfig class updated

--- u3SportCalendar/AppConfig.py
+++ u3SportCalendar/AppConfig.py
@@ -2,24 +2,59 @@ import json
 
 class AppConfig:
     def __init__(self):
-        self.events_calendar = None
+        self.config_file_name = "config.json"
+        self.default_events_calendar = "primary"
+        self.events_calendar = self.default_events_calendar
+        self.default_how_many_days = 14
+        self.how_many_days = self.default_how_many_days
+        self.default_update_hour = True
+        self.update_hour = self.default_update_hour
 
     def save(self):
         data = {
-            "events_calendar": self.events_calendar
+            "events_calendar": self.events_calendar,
+            "how_many_days": self.how_many_days,
+            "update_hour": self.update_hour
         }
-        with open("config.json", "w") as jsonfile:
+        with open(self.config_file_name, "w") as jsonfile:
             json.dump(data, jsonfile)
             jsonfile.close()
 
     def load(self):
-        with open("config.json", "r") as jsonfile:
+        with open(self.config_file_name, "r") as jsonfile:
             data = json.load(jsonfile)
-            self.events_calendar = data["events_calendar"]
+            if ("events_calendar" in data):
+                self.events_calendar = data["events_calendar"]
+            if ("how_many_days" in data):
+                self.how_many_days = data["how_many_days"]
+            if ("update_hour" in data):
+                self.update_hour = data["update_hour"]
             jsonfile.close()
 
     def get_events_calendar(self) -> str:
-        return self.events_calendar
+        if (self.events_calendar is None or self.events_calendar == ""):
+            return self.default_events_calendar
+        else:
+            return self.events_calendar
 
-    def set_events_calendar(self, calendar):
-        self.events_calendar = calendar
+    def set_events_calendar(self, calendar:str):
+        if (calendar is not None and calendar != ""):
+            self.events_calendar = calendar
+
+    def get_how_many_days(self) -> int:
+        if (self.how_many_days is not None and self.how_many_days > 0):
+            return self.how_many_days
+        else:
+            return self.default_how_many_days
+    
+    def set_how_many_days(self, days:int):
+        if (days is not None and days > 0):
+            self.how_many_days = days
+
+    def get_update_hour(self) -> bool:
+        return self.update_hour
+
+    def set_update_hour(self, update:bool):
+        self.update_hour = update
+
+    
